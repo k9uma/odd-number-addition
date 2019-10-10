084082a1458051ff6ad38003a81c3d3c353d3093
Dennis Mubamba <dennis.mubamba@mikaexpress.com>
2019-10-10 12:51:07 +0200

    added commnents explaining the algorithm

--- src/main/NumberCheck.java
+++ src/main/NumberCheck.java
@@ -11,14 +11,21 @@ public class NumberCheck {
 		
         // Declare Array of Numbers to Be Checked
         Integer[] temp = {1, 3, 5, 7, 9, 11, 13, 15};
-
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(temp));
 
         //This randomly picks any number in the arraylist as opposed to moving index by index
         Random rand = new Random(numbers.size());
 
         //TODO Create Algorithm
-        int curr_result = 0;
+       
+		/**
+		 * So Algorithm works in a way that, 3 random numbers are picked from the arraylist and 
+		 * are added together to see if the combination can give us 30.
+		 * So the loop will continue until curr_result is equal to 30.
+		 * PS watch out for the loop, it might make you pc hang. You will see why :)
+		 */
+
+		int curr_result = 0;
 
         while (true) {
 
