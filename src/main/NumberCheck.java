be53937942d7b24149b9ea15e21f4b7f76ca1d8c
moksnow <khandanmoh@gmail.com>
2019-10-19 13:11:41 +0330

    add output for when no triplet found

--- src/main/NumberCheck.java
+++ src/main/NumberCheck.java
@@ -7,76 +7,76 @@ import java.util.HashSet;
 
 public class NumberCheck {
 
-	public static void main(String[] args) {
-		int[] A = {1, 3, 5, 7, 9, 11, 13, 15};
-		int sum = 30;
-		int arr_size = A.length;
-		findNumbers(A, arr_size, sum);
-	}
+    public static void main(String[] args) {
+        int[] A = {1, 3, 5, 7, 9, 11, 13, 15};
+        int sum = 30;
+        int arr_size = A.length;
+        findNumbers(A, arr_size, sum);
+    }
 
-	/**
-	 * optimization number checking
-	 */
-	private static void findNumbers(int[] A, int arr_size, int sum) {
-		// Fix the first element as A[i]
-		for (int i = 0; i < arr_size - 2; i++) {
+    /**
+     * optimization number checking
+     */
+    private static void findNumbers(int[] A, int arr_size, int sum) {
+        // Fix the first element as A[i]
+        for (int i = 0; i < arr_size - 2; i++) {
 
-			// Find pair in subarray A[i+1..n-1]
-			// with sum equal to sum - A[i]
-			HashSet<Integer> s = new HashSet<>();
-			int curr_sum = sum - A[i];
-			for (int j = i + 1; j < arr_size; j++) {
-				if (s.contains(curr_sum - A[j]) && curr_sum - A[j] != (int) s.toArray()[s.size() - 1]) {
-					System.out.printf("Triplet is %d, %d, %d", A[i], A[j], curr_sum - A[j]);
-					return;
-				}
-				s.add(A[j]);
-			}
-		}
+            // Find pair in subarray A[i+1..n-1]
+            // with sum equal to sum - A[i]
+            HashSet<Integer> s = new HashSet<>();
+            int curr_sum = sum - A[i];
+            for (int j = i + 1; j < arr_size; j++) {
+                if (s.contains(curr_sum - A[j]) && curr_sum - A[j] != (int) s.toArray()[s.size() - 1]) {
+                    System.out.printf("Triplet is %d, %d, %d", A[i], A[j], curr_sum - A[j]);
+                    return;
+                }
+                s.add(A[j]);
+            }
+        }
+        // If we reach here, then no triplet was found
+        System.out.println("no triplet is found...");
 
-		// If we reach here, then no triplet was found
-	}
+    }
 
-	private static void findNumbersO3() {
-		// Declare Array of Numbers to Be Checked
+    private static void findNumbersO3() {
+        // Declare Array of Numbers to Be Checked
 
-		Integer[] temp = {1, 3, 5, 7, 9, 11, 13, 15};
+        Integer[] temp = {1, 3, 5, 7, 9, 11, 13, 15};
 
-		ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(temp));
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(temp));
 
-		//Reverse the list
-		Collections.sort(numbers, Collections.reverseOrder());
+        //Reverse the list
+        Collections.sort(numbers, Collections.reverseOrder());
 
-		//TODO Create Algorithm
+        //TODO Create Algorithm
 
-		int $ideal_result = 30;
+        int $ideal_result = 30;
 
-		int $curr_result = 0;
+        int $curr_result = 0;
 
-		for (int i = 0; i < numbers.size(); i++)
-		{
-			for (int j = i + 1; j < numbers.size(); j++) {
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = i + 1; j < numbers.size(); j++) {
 
-				for (int k = i + 1; k < numbers.size(); k++) {
-					$curr_result = numbers.get(i) + numbers.get(j) + numbers.get(k);
+                for (int k = i + 1; k < numbers.size(); k++) {
+                    $curr_result = numbers.get(i) + numbers.get(j) + numbers.get(k);
 
-					System.out.println("Result:" + $curr_result);
+                    System.out.println("Result:" + $curr_result);
 
-					if($curr_result == $ideal_result) {
+                    if ($curr_result == $ideal_result) {
 
-						break;
+                        break;
 
-					}
+                    }
 
-				}
+                }
 
-			}
+            }
 
-			//System.out.println(numbers.get(i));
-		}
+            //System.out.println(numbers.get(i));
+        }
 
-		//System.out.println("Hello World");
-	}
+        //System.out.println("Hello World");
+    }
 
 
 }
